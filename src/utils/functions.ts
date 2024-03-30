6cf6a3156c4f0689dafd0b061c6840e7d9f6c290
newt239 <ysti2019.win2020.st@gmail.com>
2024-03-30 12:14:27 +0900

    feat: show next bus

---
+++ src/utils/functions.ts
@@ -0,0 +1,88 @@
+import dayjs from "dayjs";
+import { BusAPIResponse, List } from "./types";
+
+export const getNextBus = async () => {
+  try {
+    const res = await fetch("http://bus.shibaura-it.ac.jp/db/bus_data.json");
+    const data: BusAPIResponse = await res.json();
+    const day = dayjs().format("D");
+    const hour = parseInt(dayjs().format("H"));
+    const minute = parseInt(dayjs().format("m"));
+    const ts_id = data.calendar[0].list.find((item) => item.day === day)?.ts_id;
+    const current_timesheet = data.timesheet.find(
+      (item) => item.ts_id === ts_id
+    );
+    if (!current_timesheet) return null;
+
+    const leftBuses = leftBusTimes(current_timesheet.list);
+    let nextLeftBus = {
+      time: "",
+      left: 0,
+    };
+    for (const bus of leftBuses) {
+      if (bus.hour > hour || (bus.hour === hour && bus.minute >= minute)) {
+        nextLeftBus = {
+          time: `${zeroPadding(bus.hour)}:${zeroPadding(bus.minute)}`,
+          left: bus.minute - minute + (bus.hour - hour) * 60,
+        };
+        break;
+      }
+    }
+
+    const rightBuses = rightBusTimes(current_timesheet.list);
+    let nextRightBus = {
+      time: "",
+      left: 0,
+    };
+    for (const bus of rightBuses) {
+      if (bus.hour > hour || (bus.hour === hour && bus.minute >= minute)) {
+        nextRightBus = {
+          time: `${zeroPadding(bus.hour)}:${zeroPadding(bus.minute)}`,
+          left: bus.minute - minute + (bus.hour - hour) * 60,
+        };
+        break;
+      }
+    }
+
+    return { nextLeftBus, nextRightBus };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const leftBusTimes = (list: List[]) => {
+  const buses = [];
+  for (const eachHour of list) {
+    if (eachHour.bus_left.num1 !== "") {
+      const eachHourBuses = eachHour.bus_left.num1.split(".").map((item) => {
+        return {
+          hour: parseInt(eachHour.time),
+          minute: parseInt(item),
+        };
+      });
+      buses.push(...eachHourBuses);
+    }
+  }
+  return buses;
+};
+
+const rightBusTimes = (list: List[]) => {
+  const buses = [];
+  for (const eachHour of list) {
+    if (eachHour.bus_right.num1 !== "") {
+      const eachHourBuses = eachHour.bus_right.num1.split(".").map((item) => {
+        return {
+          hour: parseInt(eachHour.time),
+          minute: parseInt(item),
+        };
+      });
+      buses.push(...eachHourBuses);
+    }
+  }
+  return buses;
+};
+
+const zeroPadding = (num: number) => {
+  return num < 10 ? `0${num}` : num;
+};
