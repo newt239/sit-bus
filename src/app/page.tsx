bdeb9e0633b0611d2138fc0422843967155a5515
newt239 <ysti2019.win2020.st@gmail.com>
2024-04-10 21:11:30 +0900

    style: improve styles when something went wrong

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,18 +20,23 @@ dayjs.tz.setDefault("Asia/Tokyo");
 export default async function Home() {
   const current = dayjs().tz("Asia/Tokyo");
   const nextBus = await getNextBus(current);
-  if (!nextBus)
+
+  if (!nextBus) {
     return (
-      <div>
+      <main className="flex h-screen flex-col items-center justify-between">
         <p>
           バス情報を取得できませんでした。
-          <a href="http://bus.shibaura-it.ac.jp/ts/today_sheet.php">
+          <a
+            href="http://bus.shibaura-it.ac.jp/ts/today_sheet.php"
+            className=" text-blue-500 hover:underline"
+          >
             公式の時刻表
           </a>
           をご確認ください。
         </p>
-      </div>
+      </main>
     );
+  }
 
   return (
     <>
